0fb946ce53924b58934fb921bab1126c0ad15cfc
Kamsnyc <kamel.singh1999@gmail.com>
2023-07-07 16:43:45 -0400

    SumArray Codewars

---
+++ SumArrays/script.js
@@ -0,0 +1,12 @@
+function sum(numbers) {
+    let total = 0;
+    for (let i = 0; i < numbers.length; i++) {
+      total += numbers[i];
+    }
+  
+    if (numbers.length <= 0) {
+      return 0;
+    }
+  
+    return total;
+  }
