5ec5b67d0a00485026ce495f4e1927d8baa7b1ba
Kamsnyc <kamel.singh1999@gmail.com>
2023-07-14 19:57:35 -0400

    codewars - Opposite Number

---
+++ oppositeNumber/script.js
@@ -0,0 +1,14 @@
+function opposite(number) {
+  if (number >= 0) {
+    return -Math.abs(number);
+  } else {
+    return Math.abs(number);
+  }
+}
+
+
+
+console.log(opposite(1))
+console.log(opposite(0))
+console.log(opposite(-127232.23232))
+console.log(opposite(-5))
